cf95a7053b17118725b3740116218fdab9925751
Baptiste JAMIN <baptistejamin@gmail.com>
2016-04-29 15:11:25 +0200

    setenv with no value

--- srcs/builtins/builtin_setenv.c
+++ srcs/builtins/builtin_setenv.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_setenv.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nathan <nathan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bjamin <bjamin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/10 14:06:11 by bjamin            #+#    #+#             */
-/*   Updated: 2016/04/19 20:13:32 by nathan           ###   ########.fr       */
+/*   Updated: 2016/04/29 15:08:56 by bjamin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,22 @@ static int	builtin_setenv_error_two_many(void)
 int			builtin_setenv(t_list *environ, char **cmds)
 {
 	t_sh	*sh;
+	char	*val;
 	int		i;
 
 	UNUSED(cmds);
 	UNUSED(environ);
 	sh = t_sh_recover();
 	i = 0;
-	if (!cmds[1] || (cmds[1] && !cmds[2]))
+	if (!cmds[1])
 		return (builtin_setenv_error_missing());
 	if (cmds[2] && cmds[3])
 		return (builtin_setenv_error_two_many());
-	if (ft_strchr(cmds[1], '=') || ft_strchr(cmds[2], '='))
+	if (ft_strchr(cmds[1], '=') || (cmds[2] && ft_strchr(cmds[2], '=')))
 		return (builtin_setenv_error_missing());
-	env_set(&sh->env_list, cmds[1], cmds[2]);
+	val = cmds[2];
+	if (!val)
+		val = ft_strnew(1);
+	env_set(&sh->env_list, cmds[1], val);
 	return (0);
 }
