3cf0ff97135e2dfbbd41ce9524a68d3bd203afd8
ngrasset <ngrasset@student.42.fr>
2016-04-29 18:18:31 +0200

    btter lexer check:

--- srcs/lexer/check_lexer.c
+++ srcs/lexer/check_lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_lexer.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nathan <nathan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ngrasset <ngrasset@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/20 14:06:48 by nathan            #+#    #+#             */
-/*   Updated: 2016/04/20 16:58:00 by nathan           ###   ########.fr       */
+/*   Updated: 2016/04/29 18:13:09 by ngrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,30 @@ static void		check_lexer_error(char *token)
 	ft_putendl("`");
 }
 
+static int		check_lexer_single_bg(t_list * t_list)
+{
+	t_token	*t;
+
+	if (!t_list)
+		return (1);
+	t = t_list->content;
+	if (!t)
+		return (1);
+	if (t->type == TO_BACKGROUND)
+	{
+		check_lexer_error("&");
+		return (1);
+	}
+	return (0);
+}
+
 int				check_lexer(t_list *token_list)
 {
 	t_token	*t;
 	t_token	*t_next;
 
 	t = NULL;
-	if (!token_list)
+	if (!token_list || check_lexer_single_bg(token_list))
 		return (1);
 	while (token_list->next)
 	{
