792c3c7cb1c974a7980ea77d56ffbfea36ad9fb2
Baptiste JAMIN <baptistejamin@gmail.com>
2016-04-30 16:34:44 +0200

    -r option on read

--- srcs/builtins/builtin_read.c
+++ srcs/builtins/builtin_read.c
@@ -6,21 +6,20 @@
 /*   By: bjamin <bjamin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/10 14:06:11 by bjamin            #+#    #+#             */
-/*   Updated: 2016/04/18 20:09:29 by bjamin           ###   ########.fr       */
+/*   Updated: 2016/04/30 16:34:09 by bjamin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <shell.h>
 
-int		builtin_read(t_list *environ, char **cmds)
+static int		builtin_read_r(char **cmds, int start)
 {
 	int		i;
 	char	*line;
 	t_sh	*sh;
 
-	i = 1;
+	i = start;
 	sh = t_sh_recover();
-	UNUSED(environ);
 	while (get_next_line(0, &line) && cmds[i])
 	{
 		env_set(&sh->vars_list, cmds[i], line);
@@ -29,4 +28,52 @@ int		builtin_read(t_list *environ, char **cmds)
 			return (0);
 	}
 	return (0);
+}
+
+static int		builtin_read_generic(char **cmds, int start)
+{
+	int		i;
+	char	*line;
+	t_sh	*sh;
+	char	*str;
+	int		should_repeat;
+
+	i = start;
+	sh = t_sh_recover();
+	str = ft_strnew(1);
+	should_repeat = 1;
+	while (should_repeat && get_next_line(0, &line))
+	{
+		if (line && ft_strlen(line) > 0 && line[ft_strlen(line) - 1] == '\\')
+		{
+			line[ft_strlen(line) - 1] = 0;
+			should_repeat = 1;
+		}
+		else
+			should_repeat = 0;
+		str = ft_strfjoin(str, line);
+	}
+	env_set(&sh->vars_list, cmds[i], str);
+	return (0);
+}
+
+int				builtin_read(t_list *environ, char **cmds)
+{
+	t_sh				*sh;
+	t_generic_options	options;
+
+	sh = t_sh_recover();
+	UNUSED(environ);
+	options = builtin_options_parser(cmds, "r");
+	if (options.error)
+	{
+		ft_putstr_fd("bad option: -", 2);
+		ft_putchar_fd(options.error_char, 2);
+		ft_putstr_fd("\n", 2);
+		return (1);
+	}
+	if (!ft_is_in(options.options, 'r'))
+		return (builtin_read_generic(cmds, options.start));
+	else
+		return (builtin_read_r(cmds, options.start));
 }
